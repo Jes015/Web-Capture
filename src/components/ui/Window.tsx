6b5d15872e82cf8c2ddd0a075c5058ccad7c65de
Jes015 <jes.18.sin.15@gmail.com>
2024-01-13 14:25:45 -0500

    feat: make the WatchRecordedWindow resizable (#26)
    close #26

--- src/components/ui/Window.tsx
+++ src/components/ui/Window.tsx
@@ -1,41 +1,43 @@
 import { SectionLayout } from '@/layouts'
 import { type SectionLayoutHeaderPropsPartial } from '@/layouts/SectionLayout/components'
 import { type BaseComponentProps } from '@/models'
-import { Rnd } from 'react-rnd'
+import { Rnd, type Props as RndProps } from 'react-rnd'
 interface WindowProps extends BaseComponentProps, SectionLayoutHeaderPropsPartial {
+  rndconfig?: RndProps
 }
 
-export const Window: React.FC<WindowProps> = ({ children, className, icon, title, rightNode }) => {
+export const Window: React.FC<WindowProps> = ({ children, className, icon, title, rightNode, rndconfig }) => {
   return (
-        <Rnd
-            default={{
-              x: 0,
-              y: 0,
-              width: 200,
-              height: 120
-            }}
-            maxWidth={200}
-            minHeight={120}
-            bounds='window'
-            enableResizing={false}
-            enableUserSelectHack
-        >
-            <SectionLayout
-                className={
-                    [
-                      'flex flex-col absolute w-full h-full',
-                      className
-                    ].join(' ')
-                }
-            >
-                <SectionLayout.Header
-                    className='select-none w-full'
-                    {...{ icon, title, rightNode }}
-                />
-                <SectionLayout.Content className='flex-grow w-full h-full'>
-                    {children}
-                </SectionLayout.Content>
-            </SectionLayout>
-        </Rnd>
+    <Rnd
+      default={{
+        x: 0,
+        y: 0,
+        width: 200,
+        height: 120
+      }}
+      maxWidth={200}
+      minHeight={120}
+      bounds='window'
+      enableResizing={false}
+      enableUserSelectHack
+      {...rndconfig}
+    >
+      <SectionLayout
+        className={
+          [
+            'flex flex-col absolute w-full h-full',
+            className
+          ].join(' ')
+        }
+      >
+        <SectionLayout.Header
+          className='select-none w-full'
+          {...{ icon, title, rightNode }}
+        />
+        <SectionLayout.Content className='flex-grow w-full h-full'>
+          {children}
+        </SectionLayout.Content>
+      </SectionLayout>
+    </Rnd>
   )
 }
