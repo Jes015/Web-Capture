53a2f742c25902019e8c4b88797320c2964b0f2e
Jes015 <jes.18.sin.15@gmail.com>
2024-01-15 10:37:00 -0500

    feat: update RecorderWindow icon (#22)
    close #22

--- src/components/feature/RecordingWindow/models/recorder.model.ts
+++ src/components/feature/RecordingWindow/models/recorder.model.ts
@@ -1,1 +1,6 @@
-export type RecordingStatus = 'on' | 'off'
+export const recordingStatusType = {
+  on: 'on',
+  off: 'off'
+} as const
+
+export type RecordingStatus = typeof recordingStatusType[keyof typeof recordingStatusType]
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'pulse-fast': 'pulse 900ms linear infinite;'
+      }
+    },
   },
   plugins: [],
 }
