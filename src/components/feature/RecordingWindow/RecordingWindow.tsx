b3159b1dbee2fc9712067d57f338bac761f54b60
DrxcoDev <156522801+DrxcoDev@users.noreply.github.com>
2024-01-28 10:20:05 +0100

    Update RecordingWindow.tsx

--- src/components/feature/RecordingWindow/RecordingWindow.tsx
+++ src/components/feature/RecordingWindow/RecordingWindow.tsx
@@ -3,13 +3,20 @@ import { type RecordWindowData } from '@/models'
 import { RecordingWindowWrap } from './components'
 import { RecorderProvider, RecorderWindowProvider } from './services/context'
 
+const NuevoComponente = () => {
+  const estiloComponente = {
+    padding: '10px', // Intenta poner el padding que te sea de tu gusto.
+    margin: '16px', // Pon aqui el margen que mas se ajuste a tus intereses.
+    // Otros estilos aquí
+  };
+
 interface RecordWindowProps {
   windowData: RecordWindowData
 }
 
 export const RecordingWindow: React.FC<RecordWindowProps> = ({ windowData }) => {
   return (
-    <RecorderWindowProvider {...{ windowData }}>
+    <RecorderWindowProvider style={estiloComponente} {...{ windowData }}>
       <RecorderProvider>
         <RecordingWindowWrap
           {...{ windowData }}
