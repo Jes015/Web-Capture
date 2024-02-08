643dd5dbcee8795f3825b05586b1c3f986b3c51f
Jes015 <jes.18.sin.15@gmail.com>
2024-02-08 17:39:06 -0500

    feat(back): add env files (#84)
    resolves #84

--- apps/back/src/app.module.ts
+++ apps/back/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SchemaEnv } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: SchemaEnv,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/back/src/config/env.config.ts
@@ -0,0 +1,11 @@
+import * as Joi from 'joi';
+
+export const SchemaEnv = Joi.object({
+  DB_NAME: Joi.string().min(3).required(),
+  DB_USERNAME: Joi.string().min(3).required(),
+  DB_PASSWORD: Joi.string().min(9).required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432).required(),
+  AUTH_SECRET: Joi.string().min(20).required(),
+  AUTH_EXPIRES: Joi.string().min(2).required(),
+}).required();
