dd538a23fce0ff0a5a088c3d355a94153988fc68
Jes015 <jes.18.sin.15@gmail.com>
2024-02-12 19:20:46 -0500

    feat(back): add helmete and csrf protection (#101)
    close #101

--- apps/back/src/main.ts
+++ apps/back/src/main.ts
@@ -1,9 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as csurf from 'csurf';
+import helmet from 'helmet';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.use(csurf());
 
   app.useGlobalPipes(
     new ValidationPipe({
